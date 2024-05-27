714c02d9662e94ef035dd3e3cd9cc66cd41a9e9b
qkqnfldTW <qkqnfld@twinny.co.kr>
2024-05-27 11:25:44 +0900

    [변경] 내부 DB에서 값을 가져오는 부분을 Flow로 변경했습니다.

--- domain/src/main/java/com/choimaro/domain/image/repository/db/BookMarkRepository.kt
+++ domain/src/main/java/com/choimaro/domain/image/repository/db/BookMarkRepository.kt
@@ -1,9 +1,10 @@
 package com.choimaro.domain.image.repository.db
 
 import com.choimaro.domain.model.ImageModel
+import kotlinx.coroutines.flow.Flow
 
 interface BookMarkRepository {
-    suspend fun getAllBookMark(): List<ImageModel>
+    suspend fun getAllBookMark(): Flow<List<ImageModel>>
     suspend fun insertBookMark(imageModel: ImageModel): Boolean
     suspend fun deleteBookMark(ids: List<String>): Boolean
     suspend fun deleteAll()
--- domain/src/main/java/com/choimaro/domain/image/usecase/db/bookmark/GetAllBookMarkUseCase.kt
+++ domain/src/main/java/com/choimaro/domain/image/usecase/db/bookmark/GetAllBookMarkUseCase.kt
@@ -2,12 +2,13 @@ package com.choimaro.domain.image.usecase.db.bookmark
 
 import com.choimaro.domain.image.repository.db.BookMarkRepository
 import com.choimaro.domain.model.ImageModel
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetAllBookMarkUseCase @Inject constructor(
     private val bookMarkRepository: BookMarkRepository
 ) {
-    suspend operator fun invoke(): List<ImageModel> {
+    suspend operator fun invoke(): Flow<List<ImageModel>> {
         return bookMarkRepository.getAllBookMark()
     }
 }
