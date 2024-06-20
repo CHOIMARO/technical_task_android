3941945c09536700cb41081ae9c45f3fe8ed3859
qkqnfldTW <qkqnfld@twinny.co.kr>
2024-06-20 17:07:01 +0900

    [추가] 화면의 반을 이미지로 처리하고, 해당하는 정보를 밑에 표시하도록 구현

--- app/src/main/java/com/choimaro/technical_task_android/home/view/screen/ImageDetailScreen.kt
+++ app/src/main/java/com/choimaro/technical_task_android/home/view/screen/ImageDetailScreen.kt
@@ -1,13 +1,26 @@
 package com.choimaro.technical_task_android.home.view.screen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.choimaro.domain.model.ImageModel
+import com.choimaro.technical_task_android.component.ImageModelItem
 import com.choimaro.technical_task_android.home.viewmodel.MainViewModel
 
 @Composable
@@ -15,7 +28,25 @@ fun ImageDetailScreen(
     imageModel: ImageModel,
     navHostController: NavHostController,
     viewModel: MainViewModel = hiltViewModel()) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(text = "ImageDetailScreen")
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(6.dp)
+    ){
+        BoxWithConstraints(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.TopEnd
+        ) {
+            val screenHeight = maxHeight
+            val halfScreenHeight = screenHeight / 2
+
+            ImageModelItem(
+                imageModel = imageModel,
+                imageModifier = Modifier
+                    .height(halfScreenHeight)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(8.dp))
+            )
+        }
     }
 }
